be496568fbe669e2e7fcfe67428c8f92f3576bca
Virgile <virgilevallvillellas@gmail.com>
2023-12-03 19:43:57 +0100

    Print a player on screen

--- game.py
+++ game.py
@@ -18,14 +18,27 @@ class Game():
         map_layer = pyscroll.orthographic.BufferedRenderer(map_data, self.screen.get_size())
         map_layer.zoom = 1
 
+        # initialiser le player
+        obj_player = tmx_data.get_object_by_name("player")
+        self.player = Player(obj_player.x, obj_player.y)
+
         # dessiner le calque
         self.group = pyscroll.PyscrollGroup(map_layer=map_layer, default_layer=2)
+        self.group.add(self.player)
 
 
 
         self.run()
 
+    def update(self):
+        self.group.update()
 
+        #self.player.fall()
+        # verif collision avec env
+        player_sprite = self.group.sprites()[0]
+        print(player_sprite)
+        if player_sprite.hitbox.collidelist(self.walls) > -1:
+            print("collide")
        
 
     def run(self):
@@ -38,8 +51,11 @@ class Game():
                     pygame.quit()
                     sys.exit()
 
+            # contour
+            pygame.draw.rect(self.player.image, (255, 0, 0), self.player.image.get_rect(), 2)
+            pygame.draw.rect(self.screen, (125, 0, 0), self.player.player_hitbox, 2)
 
-
+            self.group.center(self.player.rect.center)
             self.group.draw(self.screen)
 
 
--- player.py
+++ player.py
@@ -6,5 +6,42 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
 
         self.tile_size = 128
-     
 
+        self.images = {
+            'left': pygame.image.load("../img/Shinobi/Run_green.png"),
+            'right': pygame.image.load("../img/Shinobi/Run_green.png"),
+            'idle': pygame.image.load("../img/Shinobi/Idle_green.png"),
+            'left': pygame.image.load("../img/Shinobi/Run_green.png"),
+            'jump': pygame.image.load('../img/Shinobi/Jump_green.png')            
+        }
+
+        self.sprite_sheet = self.images['idle']
+        self.image = self.get_image(0, 0)           #Surface 128x128
+        self.image.set_colorkey([0, 255 ,0])
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        print("self.rect" , self.rect)
+
+        self.player_hitbox = pygame.Rect(0, 0, self.rect.width * 0.5, 100)
+        print("self.player_hitbox", self.player_hitbox)
+
+        #self.rect.topleft = self.position
+        print("self.player_hitbox.midbottom", self.player_hitbox.midbottom)
+        print("self.rect.midbottom", self.rect.midbottom)
+        self.player_hitbox.midbottom = self.rect.midbottom
+
+
+        
+
+        
+
+
+    def update(self):
+        # Logique de mise à jour du joueur (si nécessaire)
+        pass
+
+    def get_image(self, x, y):
+        image = pygame.Surface([self.tile_size, self.tile_size])
+        image.blit(self.sprite_sheet, (0, 0), (x, y, self.tile_size, self.tile_size))
+        return image
